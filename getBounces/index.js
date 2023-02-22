8019f114ea5433b9e0f7e7248475a69bcf8fb34e
Joe Marcus <josephcmarcus@gmail.com>
2023-02-22 12:25:33 -0500

    Create call to Postmark API to obtain bounce data

--- getBounces/index.js
+++ getBounces/index.js
@@ -1,14 +1,39 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an orchestrator function.
- * 
- * Before running this sample, please:
- * - create a Durable orchestration function
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your
- *   function app in Kudu
- */
+﻿const dotenv = require('dotenv').config();
+const axios = require('axios');
+const getYesterdayDate = require('../utils/getYesterdayDate');
 
 module.exports = async function (context) {
-    return;
+  const { instanceId } = context.bindingData.args;
+  const yesterday = getYesterdayDate();
+
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `${process.env.POSTMARK_BASE_URL}`,
+      params: {
+        type: 'HardBounce',
+        count: 500,
+        inactive: 'true',
+        offset: 0,
+        fromdate: `${yesterday}T00:00:00`,
+        todate: `${yesterday}T23:59:59`,
+      },
+      headers: {
+        Accept: 'application/json',
+        'X-Postmark-Server-Token': process.env.POSTMARK_SERVER_TOKEN,
+      },
+    });
+
+    if (response.data.TotalCount === 0) {
+        context.log(`getBounces succeeded for ID = '${instanceId}'. No bounces found.`);
+      return null;
+    }
+
+    context.log(`getBounces succeeded for ID = '${instanceId}'. Bounces received: ${response.data.TotalCount}.`);
+
+    return response.data.Bounces;
+  } catch (err) {
+    context.log(`getBounces failed for ID = '${instanceId}'. ${err}`);
+    return null;
+  }
 };
--- postmark-bounce-logger/index.js
+++ postmark-bounce-logger/index.js
@@ -1,22 +1,18 @@
-﻿/*
- * This function is not intended to be invoked directly. Instead it will be
- * triggered by an HTTP starter function.
- * 
- * Before running this sample, please:
- * - create a Durable activity function (default name is "Hello")
- * - create a Durable HTTP starter function
- * - run 'npm install durable-functions' from the wwwroot folder of your 
- *    function app in Kudu
- */
-
-const df = require("durable-functions");
+﻿const df = require("durable-functions");
+const getYesterdayDate = require('../utils/getYesterdayDate');
 
 module.exports = df.orchestrator(function* (context) {
-    const activityPayload = {};
-    const outputs = [];
+    const activityPayload = {
+        instanceId: context.df.instanceId,
+        bounces: [],
+    };
 
-    outputs.push(yield context.df.callActivity("getBounces", activityPayload));
-    outputs.push(yield context.df.callActivity("writeBounces", activityPayload));
+    const bounces = yield context.df.callActivity("getBounces", activityPayload);
+    if (bounces === null) {
+        context.log(`No bounces found for ${instanceId}. Exiting function.`)
+    }
 
-    return outputs;
+    activityPayload.bounces = bounces;
+
+    return activityPayload;
 });
