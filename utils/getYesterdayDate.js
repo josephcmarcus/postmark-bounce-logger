2c17259b3ef9b4ce4709d6549bc812a199b4d663
Joe Marcus <josephcmarcus@gmail.com>
2023-02-22 12:25:02 -0500

    Create utility function to obtain yesterday's date

---
+++ utils/getYesterdayDate.js
@@ -0,0 +1,6 @@
+module.exports = function getYesterdayDate() {
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(yesterday.getDate() - 1);
+  return yesterday.toISOString().split('T')[0];
+}
